7741e88c2f92cd991503951cb345eb2f8903f0eb
realfatihciftci <fatih.ciftci@std.bogazici.edu.tr>
2026-04-05 20:18:20 +0300

    Kategoriler dinamik açılıyor tamamlandı.

--- CetBookStore/Components/CategoryMenuViewComponent.cs
+++ CetBookStore/Components/CategoryMenuViewComponent.cs
@@ -1,6 +1,22 @@
-namespace CetBookStore.Components;
+using CetBookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-public class CategoryMenuViewComponent
+namespace CetBookStore.Components
 {
-    
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryMenuViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        } 
+    }
 }
+
